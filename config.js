2471438707d94fe3d5428576f36d74b00b8ae21e
luluwuluying <lxw378@miami.edu>
2017-05-02 14:32:31 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    "use strict";
-
-    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "AIzaSyA1xOIznkSi_MVvl1ktOreqbsZzB0_PW8g";
-    exports.outputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || "json";
-
-})();
